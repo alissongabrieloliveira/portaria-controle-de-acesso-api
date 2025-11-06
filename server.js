1c70bd9ca8e42bbe8221dee510de8a938aa45f23
Alisson Gabriel <a7.goliveira@gmail.com>
2025-11-06 20:00:16 -0300

    chore: estrutura inicial do projeto e configuração da conexão com PostgreSQL

---
+++ server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+require("./src/config/db");
+const app = require("./src/app");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
